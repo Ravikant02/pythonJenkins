afc7d6049282bc72329e1cdc60697b7127a494a9
Ravikant02 <ravikant.software@gmail.com>
2015-06-25 12:24:46 +0530

    removed get_name method

--- loops.py
+++ loops.py
@@ -3,7 +3,3 @@ __author__ = 'inwiter'
 
 def name():
     return "Python"
-
-
-def get_name(str_name):
-    return str_name
--- tests.py
+++ tests.py
@@ -6,8 +6,3 @@ from loops import *
 def test_name():
     name_str = name()
     assert name_str == "Python"
-
-
-def test_get_name():
-    str_name = get_name("Ravikant Verma")
-    assert str_name == "Ravikant Verma"
